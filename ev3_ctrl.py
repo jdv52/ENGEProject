db12b6134cfc3867b2f4e309b87e481e1f150fd5
Jayson De La Vega <66391744+jdv52@users.noreply.github.com>
2022-04-19 17:27:27 -0400

    Update ev3_ctrl.py

--- ev3_ctrl.py
+++ ev3_ctrl.py
@@ -12,7 +12,6 @@
 import rospy
 from std_msgs.msg import Int16
 from geometry_msgs.msg import Twist
-from ev3dev2.motor import MoveTank, OUTPUT_A, OUTPUT_B
 
 # TODO: Measure and adjust these values
 WHEEL_DISTANCE 1
@@ -21,36 +20,6 @@ WHEEL_RAD 1
 # ROS nodes to publish left and right motor encoder ticks
 r_pub = rospy.Publisher('/right_ticks', Int16)
 l_pub = rospy.Publisher('/left_ticks', Int16)
-
-right_motor = LargeMotor(OUTPUT_A)
-left_motor = LargeMoror(OUTPUT_B)
-right_wheel_ticks = 0
-left_wheel_ticks  = 0
-
-def callback(data):
-	"""
-	Callback function for subscriber node receiving data
-
-	This function is called when the node subscribed to '/cmd_vel' receives
-	a message. This function utilizes the linear x-direction velocity and
-	angular z-direction velocity to calculate the robot's left and right
-	wheel speeds.
-
-	Parameters
-	----------
-	data : geometry_msgs.msg.Twist
-		Message with two 3D Vectors defining linear and angular velocity.
-
-	"""
-	linear_vel = data.linear.x
-	angular_vel = data.angular.z
-	
-	# Algorithm based on kinematic model of non-holonomic 2-wheeled robot
-	left_speed = (linear_vel - WHEEL_DISTANCE * angular_vel) / WHEEL_RAD 
-	right_speed = (linear_vel + WHEEL_DISTANCE * angular_vel) / WHEEL_RAD
-	
-	# TODO: remap "speed" values to percentages of maximum wheel speed
-	# TODO: drive motor at resulting speed percentage
 
 def control():
 	"""
@@ -67,12 +36,12 @@ def control():
 
 	"""
 	rospy.init_node('ev3_ctrl', anonymous=True)
-	rospy.Subscriber('/cmd_vel', Twist, callback)
+	rate = rospy.Rate(10)
 
-	r_pub.publish(right_motor.position)
-	l_pub.publish(left_motor.position)
-
-	rospy.spin()
+	while not rospy.is_shutdoown():
+		r_pub.publish(right_motor.position)
+		l_pub.publish(left_motor.position)
+		rate.sleep()
 
 if __name__ == '__main__':
 	try:
